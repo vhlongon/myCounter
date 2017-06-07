c747b917ae0f98174f7782da4488935eabda8645
Your Name <conradkay123@gmail.com>
2017-06-06 20:54:02 -0400

    update each counter to have unique id

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = ({Counters}) => (
   <div>
-    
+
   </div>
 )
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+let id = 0;
 const Counter = ({ value, onIncrement, onDecrement}) => (
 	<div className="counter">
 		<h1>{value}</h1>
-		<button onClick={onIncrement}>+</button>
-		<button onClick={onDecrement}>-</button>
+		<button onClick={function(){id++, onIncrement}} id={id}>+</button>
+		<button onClick={function(){id++, onDecrement}} id={id}>-</button>
 	</div>
 )
 
 export default Counter;
 //check
+console.log(id);
