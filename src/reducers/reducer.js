8d68873eab26d482c9682cdbd7117571ef8592dc
Your Name <conradkay123@gmail.com>
2017-06-11 11:36:55 -0400

    fix reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,14 @@
 import {combineReducers} from 'redux';
 
-const reducer = (state = 0, action) => {
+const initialState = {
+  counters: [
+    {
+      id: 0,
+      value: 0
+    }
+  ]
+}
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return state.value + 1; //or state + 1? or object.assign({},state, {value + 1})
@@ -12,7 +20,7 @@ const reducer = (state = 0, action) => {
 }
 
 
-const newCounterReducer = (state = {}, action) => {
+const newCounterReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'NEWCOUNTER':
       return Object.assign({}, state, {
