3cf2eb90c8b425e35c8a3ed0810366f3e04bebf8
Your Name <conradkay123@gmail.com>
2017-06-14 16:18:26 -0400

    solve conflict

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,16 +1,14 @@
 import { connect } from 'react-redux';
-import Counter from '../components/Counter';
+  import Counter from '../components/Counter';
 
-//gives you component the redux state.
+  //gives component function on how to dispatch.
+  const mapDispatchToProps = (dispatch) => {
+  	return {
+      onIncrement: (id) => dispatch({type: 'INCREMENT', id}),
+      onDecrement: (id) => dispatch({type: 'DECREMENT', id})
+    }
+  }
 
-//gives component function on how to dispatch.
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onIncrement: (id) => dispatch({type: 'INCREMENT', id}),
-		onDecrement: (id) => dispatch({type: 'DECREMENT', id})
-	}
-}
 
 const CounterContainer = connect(null, mapDispatchToProps)(Counter);
-//react-redux automatically makes a container, this is the cool-kid way
 export default CounterContainer;
