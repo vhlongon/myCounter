0dc58ef5abd16ed649399512b2e7277f99a3071c
Your Name <conradkay123@gmail.com>
2017-06-14 16:19:30 -0400

    solve conficts

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-  import Counter from '../components/Counter';
+import Counter from '../components/Counter';
 
   //gives component function on how to dispatch.
   const mapDispatchToProps = (dispatch) => {
