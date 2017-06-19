966a3a09655a2520582ff9e22b25813043be0348
Your Name <conradkay123@gmail.com>
2017-06-18 21:11:26 -0400

    delete feature done, v6.0 :)

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import CounterContainer from '../containers/CounterContainer';
 //import Counter from './Counter';
 
-const App = ({counters, onNewToDo, allValue}) => {
-  return (
-    <div>
-      <h1>{allValue}</h1>
-      <button onClick={onNewToDo}>Add Counter</button>
-      { counters.counterList.map(counter =>
-        <CounterContainer key={counter.id} id={counter.id} value={counter.value} />
-      )}
-
-    </div>
-  )
-}
+const App = ({counters, onNewToDo, allValue}) => (
+  <div>
+    <h1>{allValue}</h1>
+    <button onClick={onNewToDo}>Add Counter</button>
+    {
+      counters.counterList.map(counter => {
+        if(counter.active) {
+          return (<CounterContainer key={counter.id} id={counter.id} value={counter.value} />)
+        }
+      })
+    }
+  </div>
+)
 export default App;
